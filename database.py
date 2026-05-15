98261bd584fe0a8809ebf1fcaf382cba8ebed438
Manisha Kumari <manisha.kumari222666@gmail.com>
2026-05-15 18:20:30 +0530

    Postgres Database query

--- database.py
+++ database.py
@@ -8,3 +8,5 @@ load_dotenv()
 DATABASE_URL = os.getenv('db_url')
 engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from models import Products
 from database import sessionLocal, engine   
 import database_models
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+
+database_models.Base.metadata.create_all(bind=engine) #Create Tables
 
 @app.get("/")
 def home():
@@ -14,53 +17,65 @@ products = [ Products(id=1, name="Notebook", description="Ltimindtree", price=20
              Products(id=2, name="Pen", description="Ltimindtree", price=5, quantity= 100),
               Products(id=3, name="Bottle", description="Ltimindtree", price=100, quantity= 500),
                Products(id=4, name="headphone", description="Ltimindtree", price=5000, quantity= 100) ]
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+         db.close()
 
-database_models.Base.metadata.create_all(bind=engine) #Create Tables
 
 def db_init():
-    db = sessionLocal()
-
-    for product in products:
-        db.add(database_models.Products(**product.model_dump()))
-
-    db.commit()
+   db = sessionLocal()
+   count = db.query(database_models.Products).count
+   if(count == 0):
+        for product in products:
+            db.add(database_models.Products(**product.model_dump()))
+        db.commit()
+    
 
 db_init()
 
 @app.get("/products")
-def get_all_products():
-    db = sessionLocal()
-    db.query()
-    return products
+def get_all_products(db: Session = Depends(get_db)):
+    db_products = db.query(database_models.Products).all()
+    return db_products
 
-@app.get("/product/{id}")
-def get_product_by_id(id: int):
-    for product in products:
-        if product.id == id:
-            return product
-    print(product)  
+@app.get("/products/{id}")
+def get_product_by_id(id: int, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Products).filter(database_models.Products.id == id).first()
+    if db_product:
+        return db_product 
     return "product not found"
 
-@app.post("/product")
-def add_product(product : Products):
-    products.append(product)
-    return products
+@app.post("/products")
+def add_product(product : Products, db: Session = Depends(get_db)):
+    db.add(database_models.Products(**product.model_dump()))
+    db.commit()
+    return product
 
-@app.put("/product/{id}")
-def update_product(id:int ,newProduct : Products):
-   for i in range(len(products)):
-    if products[i].id == id:
-        products[i] = newProduct
-        return "updated successfully"
-
-    return "product not found"
-
-@app.delete("/product/{id}")
-def delete_product(id:int):
-    for i in range(len(products)):
-        if products[i].id == id:
-            del products[i]
-            return "Product Deleted"
-    return "Product not found"
+@app.put("/products/{id}")
+def update_product(id:int ,newProduct : Products, db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Products).filter(database_models.Products.id == id).first()
+    if db_product:
+        db_product.id = newProduct.id
+        db_product.name = newProduct.name
+        db_product.description = newProduct.description
+        db_product.price = newProduct.price
+        db_product.quantity = newProduct.quantity
+        db.commit()
+        return "Product Updated"
+    else:
+        return "product not found"
+    
+@app.delete("/products/{id}")
+def delete_product(id:int,  db: Session = Depends(get_db)):
+    db_product = db.query(database_models.Products).filter(database_models.Products.id == id).first()
+    if db_product:
+        db.delete(db_product)
+        db.commit()
+        return "Product Deleted"
+    else:
+        return "Product not found"
         
 
